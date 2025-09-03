a6c8f423b45ba6242ebe7d0ee41435312b2b1606
Sancy061 <Yeison.S.Sanchez.-ND@disney.com>
2025-09-02 20:59:22 -0500

    observable unsuscriptions

--- src/components/next-level-panel/next-level-panel.ts
+++ src/components/next-level-panel/next-level-panel.ts
@@ -1,23 +1,33 @@
 import { gameState$, GameState } from '../alphabet-invasion-game/game-state/game-state';
 
+import { Subscription } from 'rxjs';
+
 type NextLevelPanel = {
     type: string;
-    startPanelScoreUpdate: () => void;
+    startNextLevelPanel: () => void;
+    stopNextLevelPanel: () => void;
 };
 
-export function NextLevelPanel() {
-    function startPanelScoreUpdate() {
+export function NextLevelPanel(): NextLevelPanel {
+    let subscription: Subscription | null = null;
+
+    function startNextLevelPanel() {
         const levelNode = document.querySelector('[level-zone]') as HTMLElement;
 
-        gameState$.subscribe((gameState: GameState) => {
+        subscription = gameState$.subscribe((gameState: GameState) => {
             const { level } = gameState;
             levelNode.textContent = String(level);
         });
     }
 
+    function stopNextLevelPanel() {
+        subscription?.unsubscribe();
+    }
+
     const state = {
         type: 'next-panel-level',
-        startPanelScoreUpdate,
+        startNextLevelPanel,
+        stopNextLevelPanel,
     };
 
     return Object.assign({}, state);
--- src/components/panel-score/panel-score.ts
+++ src/components/panel-score/panel-score.ts
@@ -1,23 +1,33 @@
 import { gameState$, GameState } from '../alphabet-invasion-game/game-state/game-state';
 
+import { Subscription } from 'rxjs';
+
 export type PanelScoreType = {
     readonly type: string;
     startPanelScoreUpdate: () => void;
+    stopPanelScoreUpdate: () => void;
 };
 
 export function PanelScore(): PanelScoreType {
+    let subscription: Subscription | null = null;
+
     function startPanelScoreUpdate() {
         const scoreNode = document.querySelector('[score-zone]') as HTMLElement;
 
-        gameState$.subscribe((gameState: GameState) => {
+        subscription = gameState$.subscribe((gameState: GameState) => {
             const { score } = gameState;
             scoreNode.textContent = String(score);
         });
     }
 
+    function stopPanelScoreUpdate() {
+        subscription?.unsubscribe();
+    }
+
     const state = {
         type: 'panel-score',
         startPanelScoreUpdate,
+        stopPanelScoreUpdate,
     };
 
     return Object.assign({}, state);
